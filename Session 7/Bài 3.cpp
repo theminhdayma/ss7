4042315f11d6b3676a66bc45bcea43490a844610
theminhdayma <147359915+theminhdayma@users.noreply.github.com>
2023-11-15 00:16:07 +0700

    BTVN ss7
    BTVN ss7

---
+++ Session 7/Bài 3.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+main(){
+	//Khai bao cac bien tien gui, lai suat, thang gui 
+	int sent_month;
+	float deposits,annual_interest_rate;
+	//Nhap cac so lieu can 
+	printf("Nhap so tien gui: ");
+	scanf("%f",&deposits);
+	fflush(stdin);
+	printf("Lai suat hang nam: ");
+	scanf("%f",&annual_interest_rate);
+	fflush(stdin);
+	printf("So thang gui: ");
+	scanf("%f",&sent_month);
+	//tinh lai va tien nhan duoc
+	float profit,money;
+	float principal=deposits;
+	for(int month=1;month<=sent_month;month++){
+		profit=principal*annual_interest_rate/12;
+		money=principal+profit;
+		principal=money;
+		printf("Thang %d\tTien gui:%.2f\tTien lai:%.2f\n",month,principal,annual_interest_rate);
+	}
+	printf("So tien nhan duoc sau thoi gian gui: %.2f\n",money);
+}
